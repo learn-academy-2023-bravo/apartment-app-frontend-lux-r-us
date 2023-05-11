c2483c5de03c9145571b1fed504c60e803e785a4
DeeMo44 <demario326@gmail.com>
2023-05-11 16:11:49 -0400

    styling for not found page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="flex items-center justify-center h-16 bg-gray-900 text-white">
+      <footer className="flex b-0 items-center justify-center h-16 bg-gray-900 text-white">
         <div className="text-center bg-gray-900">
           <p>Developed By: Felix, Borja, and DeMario</p>
         </div>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,51 +1,56 @@
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import right from "../assets/right.png";
+import left from "../assets/left.png";
+import top from "../assets/top.png";
+import bottom from "../assets/bottom.png";
+import front from "../assets/front.png";
+import back from "../assets/back.png";
 
 const NotFound = () => {
-  const loader = new THREE.CubeTextureLoader();
-  const texture = loader.load([
-    "right.jpg",
-    "left.jpg",
-    "top.jpg",
-    "bottom.jpg",
-    "front.jpg",
-    "back.jpg",
-  ]);
+  const canvasRef = useRef(null);
 
-  const scene = new THREE.Scene();
-  scene.background = texture;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.set(0, 0, 0);
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([right, left, top, bottom, front, back]);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
+    scene.background = texture;
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-  const cube = new THREE.Mesh(geometry, material);
-  cube.position.set(0, 0, -5);
-  scene.add(cube);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(0, 0, 0);
 
-  function animate() {
-    requestAnimationFrame(animate);
-    // Rotate the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
+    // const geometry = new THREE.BoxGeometry();
+    // const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    // const cube = new THREE.Mesh(geometry, material);
+    // cube.position.set(0, 0, -5);
+    // scene.add(cube);
 
-  animate();
+    function animate() {
+      requestAnimationFrame(animate);
+      // Rotate the cube
+      camera.rotation.x += 0.001;
+      camera.rotation.y += 0.001;
+      renderer.render(scene, camera);
+    }
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    animate();
+  }, []);
 
   return (
-    <>
-      <h1>this is the not found page</h1>
-    </>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <canvas ref={canvasRef} />
+    </div>
   );
 };
 
